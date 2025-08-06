63ef418365473d58ef254aad18614316f391b1c1
mohitmonnappa <mohitmonnappa@gmail.com>
2025-08-06 11:04:04 -0500

    Created a broadcast ARP packet

--- network_scanner.py
+++ network_scanner.py
@@ -1,11 +1,14 @@
 #!/bin/python3
 from scapy import all as scapy
 
+
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_broadcast = broadcast/arp_request # / is used to append
+    print(arp_broadcast.summary()) # show() can be used to show all details
 
 scan("192.168.68.0/24")
 
 # Output:
-# ARP who has Net("192.168.68.0/24") says 192.168.68.54
+# Ether / ARP who has Net("192.168.68.0/24") says 192.168.68.54
