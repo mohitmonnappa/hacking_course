fe65da09f6846b44c95422a59622400bb8a8ddd9
mohitmonnappa <mohitmonnappa@gmail.com>
2025-08-06 10:15:30 -0500

    Basic ARP Request

--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,11 @@
 #!/bin/python3
-
-import scapy.all as scapy
+from scapy import all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
 
 scan("192.168.68.0/24")
+
+# Output:
+# ARP who has Net("192.168.68.0/24") says 192.168.68.54
