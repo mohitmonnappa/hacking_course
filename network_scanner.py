bab505e7410dfda7a3aab36e46a53e0d67731d93
mohitmonnappa <mohitmonnappa@gmail.com>
2025-08-07 11:32:00 -0500

    Parsed all the results, displayed neatly

--- network_scanner.py
+++ network_scanner.py
@@ -7,12 +7,23 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_broadcast = broadcast/arp_request # / is used to append
     # srp returns 2 lists by default: answered packets and unanswered packets
-    answered = scapy.srp(arp_broadcast, timeout=1)[0] # srp sends packets with our user defined frame
-    print(answered.summary())
+    answered_packets = scapy.srp(arp_broadcast, timeout=1, verbose=False)[0 ] # srp sends packets with our user defined frame
+    clients_list = []
+    for i in answered_packets:
+        dict_answered_packets = {}
+        ip, mac = str(i[1].psrc), str(i[1].hwsrc)
+        dict_answered_packets["ip"], dict_answered_packets["mac"] = ip, mac
+        clients_list.append(dict_answered_packets)
+    return clients_list
 
+def print_results(results_list):
+    print("IP\t\t\tMAC")
+    for client in results_list:
+        print(client['ip'],"\t", client['mac'])
 
 scan("192.168.68.0/24")
 
 # Output:
-# Ether / ARP who has 192.168.68.1 says 192.168.68.60 ==> Ether / ARP is at 3c:64:cf:36:b4:a8 says 192.168.68.1
-# Ether / ARP who has 192.168.68.57 says 192.168.68.60 ==> Ether / ARP is at 3c:a3:08:00:b9:19 says 192.168.68.57
+# IP			            MAC
+# 192.168.68.1 	     3c:64:cf:36:b4:a8
+# 192.168.68.56 	 a8:51:ab:d5:99:07
